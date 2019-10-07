958e778c866e76575c52e1d52e06c682eddab349
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-07 11:08:19 -0400

    add of my strcpy

---
+++ my_strcpy.c
@@ -0,0 +1,13 @@
+char *my_strcpy(char *dest,char *src)
+{    
+    int i;
+    
+    i = 0;
+    while (dest[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    return (dest);
+}            
+          
+ 
