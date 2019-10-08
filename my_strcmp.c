d698c0155513a1ffd7adcd0a97b5d66e7c76ff10
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-08 13:24:50 -0400

    add of new my_strstr.c

--- my_strcmp.c
+++ my_strcmp.c
@@ -7,16 +7,16 @@
 
 int my_strcmp(char const *s1, char const *s2)
 {
-    int i = 0;
+    int k = 0;
 
-    while ((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0')) {
-        i++;
+    while (s1[k] == s2[k] && s1[k] != '\0' && s2[k] != '\0') {
+        k++;
     }
-    if (s1[i] < s2[i]) {
+    if (s1[k] < s2[i]) {
         return (-1);
     }
 
-    else if (s1[i] > s2[i]) {
+    else if (s1[k] > s2[k]) {
         return (1);
     }
     return (0);
