983a118cbfe8dc23b246ad58153311cca9dd6609
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-08 05:55:37 -0400

    git add my_strncpy

--- my_strncpy.c
+++ my_strncpy.c
@@ -9,11 +9,18 @@
 char *my_strncpy(char *dest, char const *src, int n)
 {
     int i;
+    int count = 0;
 
+    for (int y = 0; dest[y] != '\0'; y++) {
+        count++;
+    }    
     i = 0;
-    while (i != n) {
+    while (i < dest[count]) {
         dest[i] = src[i];
         i++;
     }
+    if ( dest[count] < n) {
+        dest[count] = '\0';
+    }    
     return (dest);
 }
