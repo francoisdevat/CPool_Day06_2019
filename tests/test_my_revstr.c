1f56268c30ab3b19c4475410da286cbad2c2e8a9
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-08 10:46:48 -0400

    add of test my revstr.c

---
+++ tests/test_my_revstr.c
@@ -0,0 +1,11 @@
+#include <criterion/criterion.h>
+
+char *my_revstr(char *str);
+
+Test (my_revstr, change_the_charactere_of_a_string)
+{
+    char str[6] = "coucu";
+
+    my_revstr(str);
+    cr_assert_str_eq (str, "ucuoc");
+}
